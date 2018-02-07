09f4d73313320ee781a8bbce2740615c0d45f05f
Luis Galaz <lgalaz@gmail.com>
2018-02-06 23:54:05 -0700

    Add Description Form

--- resources/assets/js/components/Uploader.vue
+++ resources/assets/js/components/Uploader.vue
@@ -3,7 +3,7 @@
         <div v-show="$refs.upload && $refs.upload.dropActive" class="drop-active">
             <h3>Suelta la imágen para cargar.</h3>
         </div>
-        <div id="content" class="flex flex-center flex-columns" v-show="!edit">
+        <div class="content flex flex-center flex-columns" v-show="! files.length && ! edit">
             <div class="mb">
                     Arrastra ó
             </div>
@@ -22,19 +22,46 @@
                 <div class="btn play-gray">
                     Selecciona tu imágen
                 </div>
-
             </vue-file-upload>
-
         </div>
+  
+        <div class="content flex flex-center flex-columns" v-if="files.length && editForm">
+            <form class="medium-width">
+                <div class="form-group">
+                    <label for="description">Agregar Descripción</label>
+                    <textarea class="form-control" id="description" aria-describedby="descriptionHelp" placeholder="bla bla bla..."></textarea>
+                    <small id="descriptionHelp" class="form-text text-muted">Acerca de la imágen.</small>
+                </div>
+            </form>
+            <div class="flex flex-center">
+                <div class="btn play-green" @click="editSave">
+                    Aceptar
+                </div>
+                <div class="btn play-gray" @click="clearDescription">
+                    Borrar
+                </div>
+            </div>       
+        </div>     
 
         <div class="upload-preview" v-show="files.length && edit">
             <div class="uploaded-image" v-if="files.length">
                 <img ref="editImage" :src="files[0].url" />
             </div>
-            <div class="options flex flex-center">
-                <div class="option" @click.prevent="editSave">Agregar Descripción</div> |
-                <div class="option" @click.prevent="$refs.upload.clear">Eliminar</div>
-            </div>
+            <template v-if="! description">
+                <div class="options flex flex-center">
+                    <div class="option" @click.prevent="showEditForm">Agregar Descripción</div> |
+                    <div class="option" @click.prevent="$refs.upload.clear">Eliminar</div>
+                </div>
+            </template>
+            <template v-else>
+                <div class="options flex flex-center flex-columns backdrop">
+                    <div class="mb medium-width text-center" v-text="description"></div>
+                    <div class="flex">
+                        <div class="btn play-green" @click.prevent="showEditForm">Editar</div>
+                        <div class="btn alert-red" @click.prevent="$refs.upload.clear">Eliminar</div>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
 </template>
@@ -47,6 +74,8 @@
             return {
                 files: [],
                 edit: false,
+                editForm: false,
+                description: ''
             }
         },
 
@@ -55,8 +84,19 @@
         },
 
         methods: {
-            editSave() {
+            showEditForm() {
                 this.edit = false;
+                this.editForm = true;
+            },
+
+            editSave() {
+                this.description = document.querySelector('#description').value;
+                this.edit = true;
+                this.editForm = false;
+            },
+
+            clearDescription() {
+                document.querySelector('#description').value ="";  
             },
 
             alert(message) {
@@ -95,40 +135,50 @@
 </script>
 
 <style lang="scss" scoped>
-
-    #dropzone, 
-    .uploaded-image,
-    .uploaded-image img{
-        height: 227px;
-        width: 406px;
-    }
-
     #dropzone {
         border: 1px dashed #CCC;
         padding: 20px;
+        height: 227px;
+        width: 406px;
 
-        #content {
+        .uploaded-image,
+        .uploaded-image img{
+            width: 364px;
+            height: 187px;
+        }
+
+        .content {
             background-color: #FAFAFA;
             height: 100%;
         }
 
+        .btn {
+            margin: 0 10px;
+        }
+        
         .options {
             position: relative;
             bottom: 35px;
             height: 35px;
             background-color: white;
-            opacity: .5;
+            opacity: .7;
             font-size: 14; 
             border-radius: unset;
+
+            &.backdrop {
+                height: 187px;
+                bottom: 187px;
+            }
+
 
             .option {
                 cursor: pointer;
                 margin: 0 10px;
                 padding: 5px;
-            }
 
-            .option:hover {
-                color: lighten(black, 20%);
+                &:hover {
+                    color: lighten(black, 50%);
+                }
             }
         }
 
